2a962311a0e9117f3610f466a1886a764840e580
cderby2 <52181290+cderby2@users.noreply.github.com>
2019-06-28 09:58:06 -0700

    Fixed mirror drawing

---
+++ TXIRayTrace.py
@@ -0,0 +1,54 @@
+import time
+import numpy as np 
+import matplotlib.pyplot as plt
+from geometry import *
+from optics import *
+
+
+primary = np.array ([0., 0., 1.], dtype=np.float32)
+PC2S  = Collimator('PC2S', (1.2500, 0.0, 731.145), 0.016, 0.055)
+M1K3  = FlatMirror('M1K3',
+                   location=(1.2500, 0.0, 735.422), size=(0.02, 0.02, 1.0),
+                   direction='x', A=-0.009, deltaA=(-0.00015, 0.00015),
+                   translation=(-0.001, 0.001), incidentNorm=primary)
+primary = M1K3.out
+print(primary)
+M2K3  = FlatMirror('M2K3',
+                   location=(1.2184, 0.0, 737.022), size=(0.02, 0.02, 1.0),
+                   direction='x', A=-0.009, deltaA=(-0.00015, 0.00015),
+                   translation=(-0.001, 0.001), incidentNorm=primary)
+
+primary = M2K3.out
+print(primary)
+PC1K3 = Collimator('PC1K3', (0.0, 0.0, 744.0), 0.008, 0.084)
+PC1K3.setXYfromMirror(M2K3)
+
+comp_list = [PC2S, M1K3, M2K3, PC1K3]
+
+fig, ax = plt.subplots(figsize=(7,4), dpi=100, facecolor='white')
+xmin =  np.inf
+xmax = -np.inf
+zmin =  np.inf
+zmax = -np.inf
+paths = []
+for comp in comp_list:
+    ax.add_collection(comp.drawZX())
+    if(xmin > comp.draw_min[0]):  xmin = comp.draw_min[0]
+    if(xmax < comp.draw_max[0]):  xmax = comp.draw_max[0]
+    if(zmin > comp.draw_min[1]):  zmin = comp.draw_min[1]
+    if(zmax < comp.draw_max[1]):  zmax = comp.draw_max[1]
+
+# set drawing range with padding
+zpad = 0.02
+dz = zmax - zmin
+zmin -= zpad * dz
+zmax += zpad * dz
+xpad = 0.02
+dx = xmax - xmin
+xmin -= xpad * dx
+xmax += xpad * dx
+ax.set_xlim(xmin=zmin, xmax=zmax)
+ax.set_ylim(ymin=xmin, ymax=xmax)
+ax.set_title('TXI SXR')
+plt.tight_layout()
+plt.show()
--- optics/FlatMirror.py
+++ optics/FlatMirror.py
@@ -73,7 +73,8 @@ class FlatMirror():
     """
     if(self.horizontal):  # horizontal reflection
       # mirror body
-      rot =g.rot2D(self.A)
+      Atotal = self.A + np.arctan(self.inp[0] / self.inp[2]) 
+      rot =g.rot2D(Atotal)
       p0 = np.array([self.loc[2], self.loc[0]])
       v1 = rot.dot([self.half_size[2], 0.])
       v2 = rot.dot([0., -self.sign * self.half_size[0]])
@@ -87,9 +88,9 @@ class FlatMirror():
 
       # mirror motion range
       #  ** this part needs to be fixed to take +/- dA **
-      dz  = self.half_size[2] * np.cos(self.A - self.dA[1])
-      dx1 = self.half_size[2] * np.sin(np.abs(self.A) + self.dA[1])
-      dx2 = self.half_size[2] * np.sin(np.abs(self.A) - self.dA[1])
+      dz  = self.half_size[2] * np.cos(Atotal - self.dA[1])
+      dx1 = self.half_size[2] * np.sin(np.abs(Atotal) + self.dA[1])
+      dx2 = self.half_size[2] * np.sin(np.abs(Atotal) - self.dA[1])
       range_segs = np.array([
                       (self.loc[2] + dz,
                        self.loc[0] + (self.trans[0] + dx1) * self.sign),
